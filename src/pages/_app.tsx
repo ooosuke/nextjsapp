3628d37a6aceabfee73b204e87a6e56354079003
ooosuke <oosuke@dissmo.biz>
2022-11-14 00:48:58 +0900

    fix: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
 import { Layout } from "@/components/Layout";
 import { Providers } from "@/providers";
 import { AppProps } from "next/app";
+import React from "react";
+import { FallbackProps, ErrorBoundary } from "react-error-boundary";
+import { QueryErrorResetBoundary } from "react-query";
+
+const ErrorFallback: React.FC<FallbackProps> = ({
+  error,
+  resetErrorBoundary,
+}) => (
+  <div>
+    <h1>Oooooops!</h1>
+    <p>{error}</p>
+    <button onClick={resetErrorBoundary}>rest</button>
+  </div>
+);
+
+const SuspenseFallback: React.FC<{}> = () => (
+  <div>
+    <p>loading...</p>
+  </div>
+);
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
-    <Providers>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Providers>
+    <React.Suspense fallback={SuspenseFallback}>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary FallbackComponent={ErrorFallback} onReset={reset}>
+            <Providers>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </Providers>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </React.Suspense>
   </>
 );
 
